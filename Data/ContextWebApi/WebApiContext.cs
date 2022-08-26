169085d45522d0ddac6406773110a8fca485116e
Vitor Daniel Mendes Dos Santos <Vitor.Santos@proway.treina>
2022-08-26 14:19:55 -0300

    [feat] entity frame

---
+++ Data/ContextWebApi/WebApiContext.cs
@@ -0,0 +1,29 @@
+﻿using Data.model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.ContextWebApi
+{
+
+    public class WebApiContext : DbContext
+    {
+        #region Props
+        public DbSet<AnimalModel> animal { get; set; }
+
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;" +
+                "Port=5436;" +
+                "Database=postgres;" +
+                "User=postgres;" +
+                "Password=123456");
+        }
+
+    }
+}
